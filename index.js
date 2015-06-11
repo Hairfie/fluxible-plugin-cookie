54f5a8da03c70fc4529c9aa6eb0adc8d6d5e4b0f
balasan <byslava@gmail.com>
2015-06-10 22:03:32 -0400

    update local cookies on setCookie

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function cookiePlugin() {
                         } else {
                             document.cookie = cookieStr;
                         }
-                        cookies = req ? req.cookies : cookie.parse(document.cookie);
+                        cookies = req ? req.cookies : cookie.parse(document.cookie);;
                     };
                     actionContext.getCookie = function (name) {
                         return cookies[name];
